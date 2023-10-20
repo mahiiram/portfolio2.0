ff2d6aaf189228fc743cd14199e60697343b7d72
mahiiram <mahendran.ramar9797@gmail.com>
2023-10-20 14:30:26 +0530

    new commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/mahiiram",
     resume:
-      "https://drive.google.com/file/d/1ATRSWBi0W8LZa_YB1b1IaYMvzRu7h_SN/view?usp=sharing",
+      "https://drive.google.com/file/d/1CZNAvK1srWOHCagtBXqIPdYwsOhuqpxI/view?usp=share_link",
     linkedin: "https://www.linkedin.com/in/mahendran-ramar9797/",
   };
   
